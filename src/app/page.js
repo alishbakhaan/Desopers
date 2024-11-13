c9f6c72b5e72c8e92f1bfdd1ab3c3f7528195619
alishbakhaan <alishbakhaan649@gmail.com>
2024-11-13 17:24:07 +0500

    production build2

--- src/app/page.js
+++ src/app/page.js
@@ -5,23 +5,17 @@ import Navbar from "./components/Navbar";
 import Portfolio from "./components/Portfolio";
 import Services from "./components/Services";
 import Technology from "./components/Technology";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Routing from "./Routes/Routing";
+
 
 export default function Home() {
   return (
    <>
-   <Router>
-      <Routes>
-        <Route path="/" element={<Routing />} />
-        <Route path="/hero" element={<Hero />} />
-        <Route path="/navbar" element={<Navbar />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/technology" element={<Technology />} />
-        <Route path="/footer" element={<Footer />} />
-      </Routes>
-    </Router>
+   <Navbar/>
+    <Hero/>
+    <Services/>
+    <Portfolio/>
+    <Technology/>
+    <Footer/>
    </>
   );
 }
